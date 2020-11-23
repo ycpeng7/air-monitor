51f183843b07a80602360ea168ef3b0f01dff0bc
ycpeng7 <yingchih.peng@gmail.com>
2020-11-23 10:15:51 -0500

    Add data ingestion

--- data-ingestion.py
+++ data-ingestion.py
@@ -1,8 +1,15 @@
-from pyspark.sql import SparkSession
+import logging
 import sys
 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
 
-def read_aq(url):
+
+
+def read_aq(url: str = ""):
+    """
+    Read air quality data from S3 and keep specific columns
+    """
     spark = SparkSession\
             .builder\
             .appName("s3 Air Quality to Spark")\
@@ -10,13 +17,21 @@ def read_aq(url):
     sc = spark.sparkContext
     sc.setLogLevel("WARN")
 
-    obj_date = '2013-11-26'
-    df = spark.read.json(url + obj_date + '/' + obj_date + '.ndjson')
-    print(df.count())
-
+    if url != "":
+        obj_date = '2014*'
+        path = url + obj_date + '/' + obj_date + '.ndjson'
+    else:
+        path = "aq_test.ndjson"
+    df = spark.read.json(path)
+    logging.warning("Read {} rows".format(df.count()))
+    df = df.select('city', 'country', 'date', 'unit', 'value')\
+        .withColumn('utc_date', df['date'].utc).drop('date')
+    
+    return df
+    
 
 def main():
-    read_aq(sys.argv[1])
+    cleaned_aq = read_aq(sys.argv[1])
 
 
 if __name__ == '__main__':
